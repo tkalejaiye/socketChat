c4c4d872b1bfe7a16753198ce6cbd620f696e692
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2017-09-06 00:03:12 +0100

    send messages to room only

--- public/js/chat.js
+++ public/js/chat.js
@@ -84,7 +84,6 @@ jQuery('#message-form').on('submit', function(e) {
 
 // sends a message to server to be created
 	socket.emit('createMessage', {
-		from: 'User',
 		text: messageTextbox.val()
 	}, function (){
 		messageTextbox.val('');
