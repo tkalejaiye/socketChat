9dd2689e1ae0588ceb1dd8d07e50840d7c4e45a5
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2017-09-05 16:42:47 +0100

    added locationmessage and locationmessage test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -10,6 +10,21 @@ describe('generateMessage', () => {
 
 		expect(message.from).toBe(from);
 		expect(message.text).toBe(text);
+		expect(message.createdAt).toBeA('number');
+	});
+
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate location object', () => {
+		var from = 'Admin';
+		var latitude = 29;
+		var longitude = 0;
+		var url = 'https://google.com/maps?q=29,0'
+		var message = generateLocationMessage(from, latitude, longitude);
+
+		expect(message.from).toBe(from);
+		expect(message.url).toBe(url);
 		expect(message.createdAt).toBeA('number');
 	});
 });
